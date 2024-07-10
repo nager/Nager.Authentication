4deda4b96132392a87f0dad7065604d0084151a0
Tino Hager <tino.hager@nager.at>
2024-07-10 11:17:46 +0200

    Optimize summary

--- src/Nager.Authentication.TestProject.WebApp/Dtos/TimeInfoDto.cs
+++ src/Nager.Authentication.TestProject.WebApp/Dtos/TimeInfoDto.cs
@@ -1,9 +1,23 @@
 ﻿namespace Nager.Authentication.TestProject.WebApp.Dtos
 {
+    /// <summary>
+    /// Time Info Dto
+    /// </summary>
     public class TimeInfoDto
     {
+        /// <summary>
+        /// Hour
+        /// </summary>
         public int Hour { get; set; }
+
+        /// <summary>
+        /// Minute
+        /// </summary>
         public int Minute { get; set; }
+
+        /// <summary>
+        /// Second
+        /// </summary>
         public int Second { get; set; }
     }
 }
